d78ced6c0d74a73ae33126133da967a05377736d
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-25 11:07:38 +0000

    Adds blog posts/tags table recipe

---
+++ joins-many-to-many/05_repository_class_many_to_many/blog/posts_tags.sql
@@ -0,0 +1,21 @@
+-- Create the first table.
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title text,
+  content text
+);
+
+-- Create the second table.
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name text
+);
+
+-- Create the join table.
+CREATE TABLE posts_tags (
+  post_id int,
+  tag_id int,
+  constraint fk_post foreign key(post_id) references posts(id) on delete cascade,
+  constraint fk_tag foreign key(tag_id) references tags(id) on delete cascade,
+  PRIMARY KEY (post_id, tag_id)
+);
